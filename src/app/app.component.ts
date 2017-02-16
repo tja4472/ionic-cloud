567ab620892bb6bc71d28eaaab4a42c966cb4487
Tim <tja2353@gmail.com>
2017-02-16 10:36:28 +0000

    loggedInPages added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,47 @@ import { TodosPage } from '../pages/todos/todos.page';
 
 import { AuthService } from '../services/auth.service';
 
+export interface PageInterface {
+  title: string;
+  component: any;
+  icon: string;
+  logsOut?: boolean;
+  index?: number;
+  tabComponent?: any;
+}
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-currentUser;
+
+  // List of pages that can be navigated to from the left menu
+  // the left menu only works after login
+  // the login page disables the left menu
+  appPages: PageInterface[] = [
+    { title: 'Page One', component: Page1, icon: 'calendar' },
+    { title: 'Page Two', component: Page2, icon: 'calendar' },
+    { title: 'Todos Page', component: TodosPage, icon: 'calendar' },
+    { title: 'Home Page', component: HomePage, icon: 'calendar' },
+    { title: 'Login', component: LoginPage, icon: 'calendar' },
+  ];
+
+  loggedInPages: PageInterface[] = [
+  ];
+
+  loggedOutPages: PageInterface[] = [
+  ];
+
+  currentUser;
   rootPage: any; // = Page1;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
   constructor(
     private authService: AuthService,
     public platform: Platform,
-    ) {
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -31,8 +58,8 @@ currentUser;
       { title: 'Page One', component: Page1 },
       { title: 'Page Two', component: Page2 },
       { title: 'Todos Page', component: TodosPage },
-      { title: 'Home Page', component: HomePage },      
-      { title: 'Login', component: LoginPage },       
+      { title: 'Home Page', component: HomePage },
+      { title: 'Login', component: LoginPage },
       // { title: 'Logout', component: LandingPage },        
     ];
 
@@ -63,12 +90,29 @@ currentUser;
 
     // check to see if there is already a user... Ionic saves it for you,
     // this will automatically log the user in when you restart the application
-    this.authService.doCheckAuth();    
+    this.authService.doCheckAuth();
   }
 
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+    isActive(page: PageInterface) {
+    let childNav = this.nav.getActiveChildNav();
+
+    // Tabs are a special case because they have their own navigation
+    if (childNav) {
+      if (childNav.getSelected() && childNav.getSelected().root === page.tabComponent) {
+        return 'primary';
+      }
+      return;
+    }
+
+    if (this.nav.getActive() && this.nav.getActive().component === page.component) {
+      return 'primary';
+    }
+    return;
   }
 }
